52721ce9c58a682b92f296e16f20f4f6637e3449
masterPle <plezikrab@gmail.com>
2017-10-22 16:00:41 +0700

    Finish 22oct

--- app/src/main/java/com/example/tanaroek_it/myeasyservice/fragment/SecondFragment.java
+++ app/src/main/java/com/example/tanaroek_it/myeasyservice/fragment/SecondFragment.java
@@ -1,5 +1,9 @@
 package com.example.tanaroek_it.myeasyservice.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.tanaroek_it.myeasyservice.R;
 
@@ -16,6 +22,8 @@ import com.example.tanaroek_it.myeasyservice.R;
 
 public class SecondFragment extends Fragment{
 
+    private ImageView imageView;
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -24,8 +32,58 @@ public class SecondFragment extends Fragment{
         //Back Controller
         backController();
 
+        //Image Controller
+        imageController();
+
+
+
 
     }   // Main Method
+
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode,
+                                 Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            try {
+
+                Uri uri = data.getData();
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Toast.makeText(getActivity(), "Not Choose Image", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+
+    private void imageController() {
+        imageView = getView().findViewById(R.id.imvHumen);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App"), 1);
+
+            }
+        });
+    }
+
+
 
     private void backController() {
         Button button = getView().findViewById(R.id.btnBack);
